38ac9a01087df50b5545ff43633f0665575e013a
Yu-Jen Chang <juzen2003@gmail.com>
2018-10-31 10:45:36 -0700

    Add pig latin logic

--- piglatin/piglatin/views.py
+++ piglatin/piglatin/views.py
@@ -4,8 +4,23 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
-# def hello(request):
-#     return HttpResponse("Hello World!")
+
 def translate(request):
     # request.GET['originaltext'] -> originaltext inputed from form is passed to translate page from request
-    return HttpResponse("You are at the translate page!" + request.GET['originaltext'])
+    originaltext = request.GET['originaltext']
+    tranlationtext = ''
+    for word in originaltext.lower().split():
+        tranlation_word = translation(word)
+        tranlationtext += tranlation_word + ' '
+    return HttpResponse(tranlationtext)
+
+# translate a word into pig latin
+def translation(word):
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    translation_word = ''
+
+    while word[0] not in vowel:
+        word = word[1:] + word[0]
+
+    word = word + 'ay'
+    return word
