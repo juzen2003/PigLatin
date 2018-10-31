0598bee649c9b085c8e6032824c481b91381620c
Yu-Jen Chang <juzen2003@gmail.com>
2018-10-31 11:38:07 -0700

    Fix the pig latin logic

--- piglatin/piglatin/urls.py
+++ piglatin/piglatin/urls.py
@@ -20,6 +20,6 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # $ for nothing
-    url(r'^$', views.home),
+    url(r'^$', views.home, name='home'),
     url(r'^translate/', views.translate, name='translate'),
 ]
--- piglatin/piglatin/views.py
+++ piglatin/piglatin/views.py
@@ -8,19 +8,26 @@ def home(request):
 def translate(request):
     # request.GET['originaltext'] -> originaltext inputed from form is passed to translate page from request
     originaltext = request.GET['originaltext']
-    tranlationtext = ''
+    translationtext = ''
     for word in originaltext.lower().split():
         tranlation_word = translation(word)
-        tranlationtext += tranlation_word + ' '
-    return HttpResponse(tranlationtext)
+        translationtext += tranlation_word + ' '
+    # return HttpResponse(translationtext)
+    return render(request, 'translate.html', {'original': originaltext, 'translation': translationtext})
 
 # translate a word into pig latin
 def translation(word):
-    vowel = ['a', 'e', 'i', 'o', 'u']
+    vowel = ['a', 'e', 'i', 'o', 'u', 'y']
     translation_word = ''
 
     while word[0] not in vowel:
         word = word[1:] + word[0]
+
+    # for idx in range(0, len(word)):
+    #     if word[0] not in vowel:
+    #         word = word[1:] + word[0]
+    #     else:
+    #         break
 
     word = word + 'ay'
     return word
